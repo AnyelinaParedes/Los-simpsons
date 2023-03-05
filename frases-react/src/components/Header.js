d926957b36d16ab2116f2454e0d4bd6e5c8000e8
AnyelinaParedes <apag15194@hotmail.com>
2023-03-05 00:14:51 -0300

    los simpsons

---
+++ frases-react/src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export default function Header() {
+  return (
+    <Title> Las mejores frases de Los Simpson</Title>
+  )
+}
+const Title = styled.h1`
+    color: #000;;
+    text-align:center;
+`;    
